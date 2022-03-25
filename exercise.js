f0657637880831f67bd81519b2add6a2eb31e1b1
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-03-25 11:04:56 -0500

    complete first problem

--- exercise.js
+++ exercise.js
@@ -1,6 +1,17 @@
+function getTotalItemSales(sales, itemID){
+    // total # of sales for that item
+    return sales.filter(item => item.itemId === itemID).map(obj => obj.quantity).reduce((sum, number) => sum + number, 0)
+
+}
+
+// getTotalItemSales(sales, itemID)
+
+
+
+
 module.exports = {
     // Uncomment these functions as you write them
-    getTotalItemSales,
-    addTotalSalesToItems,
-    addTotalValueToItems
+     getTotalItemSales,
+   // addTotalSalesToItems,
+   // addTotalValueToItems
 }
