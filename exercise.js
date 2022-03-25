8a48b9efe6ec7db1156f572083c89110a511b649
Brad Slagle <btslagle@gmail.com>
2022-03-25 15:19:53 -0500

    Attempt #3

--- exercise.js
+++ exercise.js
@@ -11,6 +11,59 @@ function addTotalSalesToItems(sales, itemList) {
     })
 }
 
+function addTotalValueToItems(sales, itemList) {
+    const variableToLog = addTotalSalesToItems(sales, itemList)
+        .map(item => {
+            let value = sales.find(e => e.quantity * item.price)
+
+            return { ...item, totalValue: value }
+
+
+        })
+        console.log(variableToLog)
+}
+
+
+    /*addTotalSalesToItems(sales, itemList).map(element =>
+                (element.price * element.quantity).toFixed(2)).map(element => element.index === item.Id)*/
+
+
+
+
+    /*const result = addTotalSalesToItems(sales, itemList)
+
+    /* const completedList = result.map(e => {
+    const value = e.totalValue
+        return {
+            ...e, totalValue: '3'}
+    
+    console.log(value)*/
+
+ /* return  addTotalSalesToItems(sales, itemList).map(element => (element.price * element.quantity).toFixed(2))*/
+
+
+
+
+
+
+
+
+    /*const quantity = result.quantity
+    const multi = result.price.reduce((result, quantity) => {
+        return result.price * quantity
+    },0)
+    console.log(multi)
+    const totalValue = quantity
+    .map(addTotalSalesToItems)
+    .reduce(getTotalItemSales,2) * quantity
+        
+    return totalValue
+  */ 
+}
+
+
+
+
 
 
 
@@ -19,5 +72,5 @@ module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
     addTotalSalesToItems,
-    // addTotalValueToItems
+    addTotalValueToItems
 }
