db4a1dcb2aaaadfe7c046e679743efaff6182256
Parnell <psteide@ford.com>
2022-03-25 13:10:35 -0400

    Complete question 2

--- exercise.js
+++ exercise.js
@@ -1,17 +1,23 @@
-function getTotalItemSales(sales, itemID){
+function getTotalItemSales(sales, itemID) {
     // total # of sales for that item
     return sales.filter(item => item.itemId === itemID).map(obj => obj.quantity).reduce((sum, number) => sum + number, 0)
 
 }
 
-// getTotalItemSales(sales, itemID)
+function addTotalSalesToItems(sales, itemList) {
+    return itemList.map(item => {
+        const quantity = getTotalItemSales(sales, item.id)
+        return { ...item, quantity }
+    })
+}
+
 
 
 
 
 module.exports = {
     // Uncomment these functions as you write them
-     getTotalItemSales,
-   // addTotalSalesToItems,
-   // addTotalValueToItems
+    getTotalItemSales,
+    addTotalSalesToItems,
+    // addTotalValueToItems
 }
